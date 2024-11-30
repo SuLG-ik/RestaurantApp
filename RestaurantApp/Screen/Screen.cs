48241372fe73f6a3f27fadaa8720b270b1d2dd48
SuLGik <58724087+SuLG-ik@users.noreply.github.com>
2024-12-01 00:26:51 +0700

    add new lifecycle Resume/Pause callbacks

--- RestaurantApp/Screen/Screen.cs
+++ RestaurantApp/Screen/Screen.cs
@@ -3,11 +3,21 @@ namespace RestaurantApp.Screen;
 public abstract class Screen
 {
     protected INavigator<Screen>? Navigator { get; private set; }
-    
+    private bool _isInitialized;
+
     public void BaseCreate(INavigator<Screen> navigator)
     {
         Navigator = navigator;
-        Create();
+        if (!_isInitialized)
+        {
+            Create();
+            Resume();
+            _isInitialized = true;
+        }
+        else
+        {
+            Resume();
+        }
     }
 
     /// <summary>
@@ -16,8 +26,23 @@ public abstract class Screen
     protected virtual void Create()
     {
     }
-    
-    
+
+    /// <summary>
+    /// Calls ones when screen shown again.
+    /// </summary>
+    protected virtual void Resume()
+    {
+    }
+
+
+    /// <summary>
+    /// Calls multiple times when screen lose context.
+    /// </summary>
+    protected virtual void Pause()
+    {
+    }
+
+
     /// <summary>
     /// Calls every time when screen is active and alive on each circle tick
     /// </summary>
@@ -25,8 +50,16 @@ public abstract class Screen
 
     public void BaseDestroy()
     {
-        Destroy();
-        Navigator = null;
+        if (Navigator?.Contains(this) == true)
+        {
+            Pause();
+        }
+        else
+        {
+            Pause();
+            Destroy();
+            Navigator = null;
+        }
     }
 
     /// <summary>
